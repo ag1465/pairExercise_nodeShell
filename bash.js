6ac6c064009b654420508661bb297129272148b3
Allen Guo <ag1465@scarletmail.rutgers.edu>
2019-08-05 13:03:08 -0400

    ls and cat

--- bash.js
+++ bash.js
@@ -1,14 +1,29 @@
 const pwd = require('./pwd');
+const ls = require('./ls');
+const fs = require('fs');
 process.stdout.write('prompt > ');
 
 process.stdin.on('data', (data) => {
-    const cmd = data.toString().trim();
+    const cmd = data.toString().trim().split(' ');
 
     // process.stdout.write('You typed: ' + cmd);
-    if (cmd === 'pwd') {
+    if (cmd[0] === 'pwd') {
 
         // process.stdout.write(process.cwd())
-        pwd()
+        pwd();
+    }
+    if (cmd[0] === 'ls'){
+        ls();
+    }
+    if (cmd[0] === 'cat'){
+        console.log(cmd[1]);
+        fs.readFile('./' + cmd[1],('utf8'),(err, content)=> {
+            if(err){throw err}
+            else{
+                process.stdout.write(content);
+            }
+        });
+
     }
     process.stdout.write('\nprompt > ');
 })
